170400157efa9b2918d08807cf86a948b9f7d733
user <user@10.2.6.135>
2019-09-10 13:34:57 -0700

    This code allows togepi to move across the board.

--- Togepi/src/TogepiThing.java
+++ Togepi/src/TogepiThing.java
@@ -1,14 +1,10 @@
-
 public class TogepiThing extends PokeThing {
-
 	public TogepiThing()
 	{
-		super("togepi");
+		super("Togepi", "",0);
+		
 	}
-	
-	public void step()
-	{
-		move(null);
+	public void step() {
+		move();
 	}
-	
 }
